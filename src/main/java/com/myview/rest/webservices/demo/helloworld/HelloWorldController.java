6ad44363f9d625d8787a7cfdfeb50f6929a2b866
rahulsahaynbs <rahul.sahay@nationwide.co.uk>
2021-10-21 20:58:14 +0530

    Added Internationalization

--- src/main/java/com/myview/rest/webservices/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/myview/rest/webservices/demo/helloworld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.myview.rest.webservices.demo.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
     //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -21,5 +26,16 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/{name}")
     public HelloWorldBean helloWorldBeanName(@PathVariable String name){
         return new HelloWorldBean( String.format("Hello world, %s", name));
+    }
+
+    /*@GetMapping(path = "/hello-world-inter")
+    public String helloWorldInter(@RequestHeader(name = "Accept-Language", required = false)
+                                              Locale locale){
+        return messageSource.getMessage("good.morning.message", null,"Default Message", locale);
+    }*/
+
+    @GetMapping(path = "/hello-world-inter")
+    public String helloWorldInter(){
+        return messageSource.getMessage("good.morning.message", null,"Default Message", LocaleContextHolder.getLocale());
     }
 }
